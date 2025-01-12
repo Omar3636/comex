c9db9a179ec4a42f209d5522603ab1855069e205
Omar3636 <yairmenaz92@gmail.com>
2025-01-12 20:10:37 -0300

    Creación de InformeSintetico.java y sobreescritura de toString

---
+++ src/main/java/com/alura/comex/InformeSintetico.java
@@ -0,0 +1,80 @@
+package com.alura.comex;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class InformeSintetico {
+    private int totalDePedidosRealizados;
+    private int totalDeProductosVendidos;
+    private int totalDeCategorias;
+    private BigDecimal montoDeVentas;
+    private Pedido pedidoMasBarato;
+    private Pedido pedidoMasCaro;
+
+    public InformeSintetico() {}
+
+    public InformeSintetico(int totalDePedidosRealizados, int totalDeProductosVendidos, int totalDeCategorias, BigDecimal montoDeVentas, Pedido pedidoMasBarato, Pedido pedidoMasCaro) {
+        this.totalDePedidosRealizados = totalDePedidosRealizados;
+        this.totalDeProductosVendidos = totalDeProductosVendidos;
+        this.totalDeCategorias = totalDeCategorias;
+        this.montoDeVentas = montoDeVentas;
+        this.pedidoMasBarato = pedidoMasBarato;
+        this.pedidoMasCaro = pedidoMasCaro;
+    }
+
+    public int getTotalDePedidosRealizados() {
+        return totalDePedidosRealizados;
+    }
+
+    public int getTotalDeProductosVendidos() {
+        return totalDeProductosVendidos;
+    }
+
+    public int getTotalDeCategorias() {
+        return totalDeCategorias;
+    }
+
+    public BigDecimal getMontoDeVentas() {
+        return montoDeVentas;
+    }
+
+    public String getMontoDeVentasString() {
+        return NumberFormat.getCurrencyInstance(new Locale("es", "CL")).format(getMontoDeVentas().setScale(2, RoundingMode.HALF_DOWN));
+    }
+
+    public Pedido getPedidoMasBarato() {
+        return pedidoMasBarato;
+    }
+
+    public String getPedidoMasBaratoString() {
+        var nombreProducto = getPedidoMasBarato().getProducto();
+        var valorFormateado = NumberFormat.getCurrencyInstance(new Locale("es", "CL")).format(getPedidoMasBarato().getPrecio().multiply(new BigDecimal(getPedidoMasBarato().getCantidad())).setScale(2, RoundingMode.HALF_DOWN));
+        return valorFormateado + " (" + nombreProducto+")";
+    }
+
+    public Pedido getPedidoMasCaro() {
+        return pedidoMasCaro;
+    }
+
+    public String getPedidoMasCaroString() {
+        var nombreProducto = getPedidoMasCaro().getProducto();
+        var valorFormateado = NumberFormat.getCurrencyInstance(new Locale("es", "CL")).format(getPedidoMasCaro().getPrecio().multiply(new BigDecimal(getPedidoMasCaro().getCantidad())).setScale(2, RoundingMode.HALF_DOWN));
+        return valorFormateado + " (" + nombreProducto+")";
+    }
+
+    @Override
+    public String toString() {
+        return """
+                #### INFORME DE VALORES TOTALES
+                - TOTAL DE PEDIDOS REALIZADOS: %s
+                - TOTAL DE PRODUCTOS VENDIDOS: %s
+                - TOTAL DE CATEGORIAS: %s
+                - MONTO DE VENTAS: %s
+                - PEDIDO MAS BARATO: %s
+                - PEDIDO MAS CARO: %s
+                """.formatted(this.totalDePedidosRealizados, this.totalDeProductosVendidos, this.totalDeCategorias, this.getMontoDeVentasString(), this.getPedidoMasBaratoString(), this.getPedidoMasCaroString());
+    }
+
+}
--- src/main/java/com/alura/comex/Main.java
+++ src/main/java/com/alura/comex/Main.java
@@ -1,11 +1,7 @@
 package com.alura.comex;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.text.NumberFormat;
 
 import java.util.*;
 
@@ -50,13 +46,7 @@ public class Main {
               categoriasProcesadas.add(pedidoActual.getCategoria());
             }
         }
-
-        System.out.println("#### INFORME DE VALORES TOTALES");
-        System.out.printf("- TOTAL DE PEDIDOS REALIZADOS: %s\n", totalDePedidosRealizados);
-        System.out.printf("- TOTAL DE PRODUCTOS VENDIDOS: %s\n", totalDeProductosVendidos);
-        System.out.printf("- TOTAL DE CATEGORIAS: %s\n", totalDeCategorias);
-        System.out.printf("- MONTO DE VENTAS: %s\n", NumberFormat.getCurrencyInstance(new Locale("es", "AR")).format(montoDeVentas.setScale(2, RoundingMode.HALF_DOWN))); //Pueden cambiar el Locale a la moneda de su pais, siguiendo esta documentación: https://www.oracle.com/java/technologies/javase/java8locales.html
-        System.out.printf("- PEDIDO MAS BARATO: %s (%s)\n", NumberFormat.getCurrencyInstance(new Locale("es", "AR")).format(pedidoMasBarato.getPrecio().multiply(new BigDecimal(pedidoMasBarato.getCantidad())).setScale(2, RoundingMode.HALF_DOWN)), pedidoMasBarato.getProducto());
-        System.out.printf("- PEDIDO MAS CARO: %s (%s)\n", NumberFormat.getCurrencyInstance(new Locale("es", "AR")).format(pedidoMasCaro.getPrecio().multiply(new BigDecimal(pedidoMasCaro.getCantidad())).setScale(2, RoundingMode.HALF_DOWN)), pedidoMasCaro.getProducto());
+        InformeSintetico informe = new InformeSintetico(totalDePedidosRealizados, totalDeProductosVendidos, totalDeCategorias, montoDeVentas, pedidoMasBarato, pedidoMasCaro);
+        System.out.println(informe);
     }
 }
